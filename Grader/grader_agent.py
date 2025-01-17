8c123e37edf975b6664ecf452d73096fca16807a
anushree0107 <anushreeghosh200@gmail.com>
2025-01-17 18:43:03 +0530

    gpt cached added

--- Grader/grader_agent.py
+++ Grader/grader_agent.py
@@ -17,7 +17,26 @@ from Retriever.kdd_retriever import KDDRulebook
 from Retriever.tmlr_retriever import TMLRRulebook
 
 set_llm_cache(InMemoryCache())
+import hashlib
+from gptcache import Cache
+from langchain.globals import set_llm_cache
+from gptcache.manager.factory import manager_factory
+from gptcache.processor.pre import get_prompt
+from langchain_community.cache import GPTCache
 
+def get_hashed_name(name):
+    return hashlib.sha256(name.encode()).hexdigest()
+
+
+def init_gptcache(cache_obj: Cache, llm: str):
+    hashed_llm = get_hashed_name(llm)
+    cache_obj.init(
+        pre_embedding_func=get_prompt,
+        data_manager=manager_factory(manager="map", data_dir=f"map_cache_{hashed_llm}"),
+    )
+
+
+set_llm_cache(GPTCache(init_gptcache))
 
 load_dotenv()
 
